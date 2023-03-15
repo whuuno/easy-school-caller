6084e521973409be869b0ae02d76bc001a028e75
Panks <pankajsingla012@gmail.com>
2023-03-15 10:43:44 +0530

    Adding RestTemplate for Consuming easy-school

--- src/main/java/com/practice/easyschoolcaller/config/ProjectConfiguration.java
+++ src/main/java/com/practice/easyschoolcaller/config/ProjectConfiguration.java
@@ -1,8 +1,10 @@
 package com.practice.easyschoolcaller.config;
 
 import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ProjectConfiguration {
@@ -10,5 +12,11 @@ public class ProjectConfiguration {
     @Bean
     public BasicAuthRequestInterceptor basicAuthRequestInterceptor(){
         return new BasicAuthRequestInterceptor("admin@eazyschool.com","123456");
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        return restTemplateBuilder.basicAuthentication("admin@eazyschool.com","123456").build();
     }
 }
--- src/main/java/com/practice/easyschoolcaller/rest/ContactController.java
+++ src/main/java/com/practice/easyschoolcaller/rest/ContactController.java
@@ -1,12 +1,15 @@
 package com.practice.easyschoolcaller.rest;
 
 import com.practice.easyschoolcaller.model.Contact;
+import com.practice.easyschoolcaller.model.Response;
 import com.practice.easyschoolcaller.proxy.ContactProxy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -17,8 +20,22 @@ public class ContactController {
     @Autowired
     ContactProxy contactProxy;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping(path = "/getMessages")
     public List<Contact> getMessagesByStatus(@RequestParam("status") String status){
         return contactProxy.getMessagesByStatus(status);
+    }
+
+    @PostMapping(path = "/saveMsg")
+    public ResponseEntity<Response> saveMsg(@RequestBody Contact contact){
+        String uri = "http://localhost:8080/api/contact/saveMsg";
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("invocationFrom","RestTemplate");
+        HttpEntity<Contact> httpEntity = new HttpEntity<>(contact, headers);
+        ResponseEntity<Response> responseEntity = restTemplate.exchange(uri, HttpMethod.POST,
+                httpEntity,Response.class);
+        return responseEntity;
     }
 }
